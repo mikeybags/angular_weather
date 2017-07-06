181bdd290fe8018dda56b7b4d9e797fbf721e785
Michael Mitchell <mikeybags@gmail.com>
2017-07-06 12:22:12 -0500

    fixed day display after service addition

--- assets/controllers.js
+++ assets/controllers.js
@@ -14,6 +14,13 @@ weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityServ
     $scope.city = cityService.city || "New York, NY";
     $scope.days = $routeParams.days || '3';
     $scope.today = new Date();
+    
+    (function setToday(today) {
+        today.setHours(0);
+        today.setMinutes(0);
+        today.setSeconds(0);
+    })($scope.today);
+    
     $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
 
     $scope.convertToFahrenheit = function(degK) {
