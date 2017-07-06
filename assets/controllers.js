641423cbb83dbfc753715122646e3d735b67c0a0
Michael Mitchell <mikeybags@gmail.com>
2017-07-06 02:31:38 -0500

    updated to form to use ng-submit

--- assets/controllers.js
+++ assets/controllers.js
@@ -1,9 +1,13 @@
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
     $scope.city = cityService.city
 
     $scope.$watch('city', function() {
         cityService.city = $scope.city;
-    })
+    });
+
+    $scope.submit = function () {
+        $location.path('/forecast')
+    }
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService',  function($scope, $resource, $routeParams, cityService) {
@@ -20,14 +24,10 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=8605d05ee34d72ccb4b362d1433bfaf1", { get: { method: "JSONP" }});
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days}, function() {
-    console.log($scope.convertToDate($scope.weatherResult.list[0].dt));
         if ($scope.convertToDate($scope.weatherResult.list[0].dt) < $scope.today) {
             $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: (parseInt($scope.days) + 1)});
         }
     });
-
-    console.log($scope.weatherResult)
-    console.log("Today is " + $scope.today)
 
     $scope.convertToFahrenheit = function(degK) {
         return Math.round((1.8 * (degK - 273)) + 32);
